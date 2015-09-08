03094391025afd19fc91414b27350616818a71f4
Bjørn Tore Gjerde <btgjerde@gmail.com>
2015-09-09 00:48:56 +0200

    Basic Cash tick cron job implemented

--- cron.py
+++ cron.py
@@ -2,7 +2,7 @@ import webapp2
 import logging
 from models import Player, Team, TeamApplication
 from google.appengine.ext import ndb
-from gameconfig import EnergyConfig
+from gameconfig import EnergyConfig, CashConfig
 
 
 class EnergyTickHandler(webapp2.RequestHandler):
@@ -19,7 +19,16 @@ class RankedTeamGamesHandler(webapp2.RequestHandler):
     def get(self):
         logging.info('Run ranked team games')
 
+class CashTickHandler(webapp2.RequestHandler):
+    def get(self):
+        players = Player.query()
+
+        for player in players:
+            player.cash += CashConfig.tickAmount
+            player.put()
+
 app = webapp2.WSGIApplication([
     (r'/cron/energy_tick', EnergyTickHandler),
-    (r'/cron/run_ranked_team_games', RankedTeamGamesHandler)
+    (r'/cron/run_ranked_team_games', RankedTeamGamesHandler),
+    (r'/cron/cash_tick', CashTickHandler)
 ], debug=True)
